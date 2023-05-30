66a2c14c64aa1790dad2b7c7cbb0d8b134a24744
akremwahabi13 <119605667+akremwahabi13@users.noreply.github.com>
2023-05-30 20:39:37 +0100

    integration

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import "./Components/map.css";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div className="container">
+        <section class="bg-gray text-dark p-5 p-lg-0 pt-lg-5 text-center text-sm-start">
+          <div class="d-sm-flex align-items-center justify-content-between">
+            <div>
+              <h1>
+                Cloud <span class="text-warning">Integration Project</span>
+              </h1>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <div className="container">
+        <h1>Contact Us</h1>
+        <ul>
+          <li>Email: akremwahabi3@gmail.com</li>
+          <li>Phone Number : 012345678910</li>
+          <il className="map-h2">Click Here</il>
+          <iframe
+            className="google-map"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12785.514435332032!2d3.0245679!3d36.7614838!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x128fb33225490931%3A0xcfc19f0953b60033!2sGoMyCode%20Algeria!5e0!3m2!1sfr!2sdz!4v1683231402843!5m2!1sfr!2sdz"
+          ></iframe>
+        </ul>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
